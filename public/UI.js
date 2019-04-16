58c136284ece3b6a1747719b26ccb9721e3d7604
trafalmejo <trafalmejo@gmail.com>
2019-04-15 20:15:26 -0400

    Horizontal Slider

--- public/UI.js
+++ public/UI.js
@@ -1,4 +1,3 @@
- var socketId = 0;
  var editor = grapesjs.init({
   height: '100%',
  	showOffsets: 1,
@@ -73,8 +72,6 @@
   	},
   }
 });
-
-
 //  editor.BlockManager.add('testInput', {
 // 	label: 'Input',
 // 	attributes: { 
@@ -83,17 +80,28 @@
 // 		category: 'Basic',
 // 		content:  '<input type="text" name="fname">'
 // 	})
-editor.BlockManager.add('testSlider', {
+editor.BlockManager.add('sliderHorizontal', {
   label: 'Slider',
-  attributes: { class:'fa fa-bars' },
+  attributes: { class:'fa fa-sliders' },
   category: 'Basic',
-  content: `<input type="range" min="1" max="100" value="50">` 
+  content: `<input type="range" min="1" max="100">` 
 })
+editor.BlockManager.add('sliderVertical', {
+  label: 'Slider',
+  attributes: { class:'fa fa-arrows-v' },
+  category: 'Basic',
+  content: `<input type="range" orient="vertical" min="1" max="100">` 
+})
+ function myFunction(){
+  console.log(wasa);
+ }
+//margin:auto to align on the center
+//inline-block to increase size depending on their children
 editor.BlockManager.add('button', {
   label: 'Button',
-  attributes: { class:'fa fa-square'},
+  attributes: { class:'fa fa-toggle-on'},
   category: 'Basic',
-  content: '<button type="button"><p style="margin: auto;">Click me!</p></button>'
+  content: '<button type="button" style="display:inline-block"><p style="margin: auto; display: inline-block;">Click me!</p></button>'
 })
   //to open the asset manager
   //editor.runCommand('open-assets');
@@ -254,8 +262,159 @@ comps.addType('button', {
   // Define the View
   //view: defaultType.view,
 });
-//button finished
+//BUTTON finished
 
+//SLIDER
+// The `input` will be the Component type ID
+comps.addType('input', {
+  // Define the Model
+  model: dModel.extend({
+    defaults: Object.assign({}, dModel.prototype.defaults, {
+      // Can't drop other elements inside it
+      droppable: false,
+      resizable: true,
+      editable: true,
+      // // Traits (Settings)
+      ip : 'localhost',
+      port: 8000,
+      message: '/slider1',
+      traits: [ 
+      {type: 'text',
+      label: 'Ip',
+      name: 'ip',
+      changeProp: 1,}, 
+      {type: 'text',
+      label: 'Port',
+      name: 'port',
+      changeProp: 1,},      
+      {type: 'text',
+      label: 'Message',
+      name: 'message',
+      changeProp: 1,},
+      {type: 'text',
+      label: 'Min',
+      name: 'min',
+      changeProp: 1,},
+      {type: 'text',
+      label: 'Max',
+      name: 'max',
+      changeProp: 1,}
+    ],
+      }),
+    init() {
+    	console.log('init: ', this);    	
+    	//this.view.ip = "";
+    	//this.view.port = "";
+    	//this.view.message = "";
+    	this.listenTo(this, 'change:port', this.changePort);
+    	this.listenTo(this, 'change:ip', this.changeIP);
+      this.listenTo(this, 'change:message', this.changeMessage);
+      this.listenTo(this, 'change:min', this.changeMin);
+    	this.listenTo(this, 'change:max', this.changeMax);
+    },
+    changeIP() {
+    	console.log("IP Changed")
+    	var newIP = this.changed.ip;
+    	if(newIP == 'localhost' || this.validateIPaddress(newIP)){
+    		console.log("Is correct");
+    		this.attributes.ip = newIP;
+
+    	}else{
+    		console.log(this);
+    		//NO WORK this.attributes.ip = "";
+    		alert("Your IP is incorrect");
+    	}
+    },
+    changePort() {
+    	var newPort = this.changed.port;
+    	console.log(this);
+    	console.log("Port Changed", newPort);
+    	if(!isNaN(parseInt(newPort))){
+    		console.log("Is correct");
+    		this.attributes.port = newPort;
+    		this.socket.emit('config', {
+    			server: { port: 4000,  host: config.ip},
+    			client: { port: newPort, host: config.ip}
+    		});
+    	}else{
+    		//NO WORK this.attributes.port = "";
+    		alert("Your port is incorrect");
+    	}
+    },
+
+    changeMessage() {
+    	console.log("Message Changed")
+    	var newMessage = this.changed.message;
+    	this.attributes.message = newMessage;
+    },
+    changeMin() {
+      var min = this.changed.min;
+      if(!isNaN(max)){
+        this.attributes.min = min;
+      }else{
+        alert("Min should be a number");
+      }
+    },
+    changeMax() {
+      var max = this.changed.max;
+      if(!isNaN(max)){
+        this.attributes.max = max;
+      }else{
+        alert("Max should be a number");
+      }
+    },
+    validateIPaddress(ipaddress) 
+    {
+    	if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipaddress)) {
+    		return (true)
+    	}
+    	return (false)
+    }
+
+  },
+  // The second argument of .extend are static methods and we'll put inside our
+  // isComponent() method. As you're putting a new Component type on top of the stack,
+  // not declaring isComponent() might probably break stuff, especially if you extend
+  // the default one.
+  {
+  	isComponent: function(el) {
+  		if(el.tagName == 'INPUT'){
+        //type text gives you text edit capabilities
+  			return {type: 'input'};
+  		}
+  	},
+  }),
+  view: dView.extend({
+    // Bind events
+    events: {
+      // If you want to bind the event to children elements
+      // 'click .someChildrenClass': 'methodName',
+//     dblclick: 'onActive',
+//     click: 'initResize',
+//     error: 'onError',
+//     dragstart: 'noDrag',
+//     mousedown: 'noDrag'
+      //input: 'changeValue',
+      input: 'changeValue',
+},
+
+    changeValue: function(e){
+      console.log("e:" , e);
+      console.log("This: ", this);
+      console.log("Value: ", this.el.value);
+      var inputValue = this.el.value;
+      var id = this.model.attributes.ip + "" + this.model.attributes.port
+      this.model.socket.emit('message', id, [this.model.attributes.message].concat(parseInt(inputValue)));
+   },
+    // The render() should return 'this'
+    render: function () {
+      // Extend the original render method
+      dView.prototype.render.apply(this, arguments);
+      return this;
+    },
+  }),
+});
+//Slider finished
 
 //editor.on('component:add', model => console.log('added ', model))
 editor.on('component:add', function(model){
@@ -270,8 +429,8 @@ editor.on('component:add', function(model){
 	console.log('Model: ',model);
 	console.log('Traits: ',model.attributes.traits);
   console.log('Type: ', model.attributes.type);
-	if(model.attributes.type == "button"){
-		console.log("It is a Button:", this);
+	if(model.attributes.type == "button" || model.attributes.type == "input"){
+		console.log("It is a Button or Input:", this);
 		model.socket = io.connect('http://'+config.ip+':8081', { port: 8081, rememberTransport: false });
 		if(true){
 			console.log("emitting config: " , model.attributes.port);
