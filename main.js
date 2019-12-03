6df3e873ae17f41593115b60751d022f98c0f586
trafalmejo <trafalmejo@gmail.com>
2019-12-02 22:11:44 -0500

    socket electron

--- main.js
+++ main.js
@@ -14,7 +14,9 @@ function createWindow () {
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
+      //preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      nodeIntegrationInWorker: true,
     }
   })
   mainWindow.maximize();
