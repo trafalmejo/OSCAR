8e07fd7e92a25328e965ebb81bb58163a6736811
trafalmejo <trafalmejo@gmail.com>
2019-12-12 17:52:13 -0500

    toggledevtools && tooltip

--- public/libraries/oscar_preview.js
+++ public/libraries/oscar_preview.js
@@ -5,78 +5,78 @@ var ipLibrary = require('ip');
 var editor;
 var ipServer = "localhost";
 
-$.get("/ipserver", function(data, status){
+$.get("/ipserver", function (data, status) {
   ipServer = data;
   initGrape();
   runPreview();
 });
 
-function runPreview(){
+function runPreview() {
   //Bug
- editor.runCommand('preview');
-}
-
-function initGrape(){
-editor =  grapesjs.init({
- // domComponents: { storeWrapper: 1 },
-  //dragMode: 'absolute',
-  height: '100%',
-  container: '#gjs-oscar-preview',
-  //fromElement: true,
-  allowScripts: 1,
-  showOffsets: false,
-  showOffsetsSelected: false,
-
-  canvas: { styles: ['assets/css/toggle.css'] },
-  // Default configurations
-  storageManager: {
-    id: 'gjs-',             // Prefix identifier that will be used on parameters
-    //type: 'local',          // Type of the storage
-    //type: null,          // Type of the storage
-    type: 'remote',          // Type of the storage
-    stepsBeforeSave: 3,     // If autosave enabled, indicates how many changes are necessary before store method is triggered
-    urlStore: 'http://'+ipServer+':8080/store',
-    urlLoad: 'http://'+ipServer+':8080/load',
-
-  },
-  // TO READ: this plugin loads default blocks
-  plugins: ['oscar_socket', 'oscar_ip', 'oscar_button', 'oscar_slider', 'gjs-preset-webpage', 'grapesjs-custom-code', 'grapesjs-parser-postcss', 'grapesjs-touch'],
-  pluginsOpts: {
-    'oscar_socket' : {
-      ipserver: ipServer,
-    },
-    'oscar_slider' : {
-      ipserver: ipServer,
-    },
-    'oscar_button': {
-      ipserver: ipServer,
-    },
-    'gjs-preset-webpage': {
-      blocks: [],
-      formsOpts: false,
-      navbarOpts: false,
-      countdownOpts: false,
-      showStylesOnChange: true,
-      modalImportTitle: 'Import Template',
-      modalImportLabel: '<div style="margin-bottom: 10px; font-size: 13px;">Paste here your HTML/CSS and click Import</div>',
-      modalImportContent: function (editor) {
-        return editor.getHtml() + '<style>' + editor.getCss() + '</style>'
-      }
-    },
-  }
-});
-//END EDITOR INIT
-editor.on('run:preview', () => {
-  const updateAll = model => {
-    model.set({editable: false,selectable:false,hoverable:false});
-    model.get('components').each(model => updateAll(model));
-}
-updateAll(editor.DomComponents.getWrapper());
-});
-editor.on('stop:preview', () => {
-  // Execute a callback on all inner components starting from the root
   editor.runCommand('preview');
-});
+}
+
+function initGrape() {
+  editor = grapesjs.init({
+    // domComponents: { storeWrapper: 1 },
+    //dragMode: 'absolute',
+    height: '100%',
+    container: '#gjs-oscar-preview',
+    //fromElement: true,
+    allowScripts: 1,
+    showOffsets: false,
+    showOffsetsSelected: false,
+
+    canvas: { styles: ['assets/css/toggle.css'] },
+    // Default configurations
+    storageManager: {
+      id: 'gjs-',             // Prefix identifier that will be used on parameters
+      //type: 'local',          // Type of the storage
+      //type: null,          // Type of the storage
+      type: 'remote',          // Type of the storage
+      //stepsBeforeSave: 1,     // If autosave enabled, indicates how many changes are necessary before store method is triggered
+      //urlStore: 'http://'+ipServer+':8080/store',
+      urlLoad: 'http://' + ipServer + ':8080/load',
+
+    },
+    // TO READ: this plugin loads default blocks
+    plugins: ['oscar_socket', 'oscar_ip', 'oscar_button', 'oscar_slider', 'gjs-preset-webpage', 'grapesjs-custom-code', 'grapesjs-parser-postcss', 'grapesjs-touch'],
+    pluginsOpts: {
+      'oscar_socket': {
+        ipserver: ipServer,
+      },
+      'oscar_slider': {
+        ipserver: ipServer,
+      },
+      'oscar_button': {
+        ipserver: ipServer,
+      },
+      'gjs-preset-webpage': {
+        blocks: [],
+        formsOpts: false,
+        navbarOpts: false,
+        countdownOpts: false,
+        showStylesOnChange: true,
+        modalImportTitle: 'Import Template',
+        modalImportLabel: '<div style="margin-bottom: 10px; font-size: 13px;">Paste here your HTML/CSS and click Import</div>',
+        modalImportContent: function (editor) {
+          return editor.getHtml() + '<style>' + editor.getCss() + '</style>'
+        }
+      },
+    }
+  });
+  //END EDITOR INIT
+  editor.on('storage:end:load', () => {
+    const updateAll = model => {
+      model.set({ editable: false, selectable: false, hoverable: false });
+      model.get('components').each(model => updateAll(model));
+    }
+    updateAll(editor.DomComponents.getWrapper());
+  });
+  editor.on('stop:preview', () => {
+    // Execute a callback on all inner components starting from the root
+    editor.runCommand('preview');
+  });
 
 }
 
--- server.js
+++ server.js
@@ -2,25 +2,22 @@
 //import '/dist/css/grapes.min.css';
 // If you need plugins, put them below the main grapesjs script
 // import 'grapesjs-some-plugin';
-
+const dotenv = require('dotenv');
+dotenv.config();
 var express = require('express')
 var app = express()
 var cors = require('cors')
 var bodyParser = require('body-parser')
 var osc = require('osc');
 //BRIDGE Between Client and Server
-//var io = require('socket.io')(8081); 
-var io = require('socket.io')(8081); 
+var io = require('socket.io')(process.env.SOCKETPORT);
 var ipLibrary = require('ip');
 var serverIP = ipLibrary.address() // my ip address
 
-var allowedOrigins = ['http://localhost:8080',
-					  'http://'+serverIP+':8080'];
 
-					  
-app.use(cors({credentials: true, origin: 'http://localhost:8080'}));
+app.use(cors({ credentials: true, origin: 'http://localhost:' + process.env.HTTPPORT }));
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 var code;
@@ -31,16 +28,16 @@ var isConnected = [];
 
 var udpPortGlobal = new osc.UDPPort({
 	localAddress: serverIP,
-	localPort: 5001,
+	localPort: process.env.LOCALPORT,
 	metadata: true,
 });
 udpPortGlobal.open()
 
 
-//localhost
+//Localhost
 var udpPortLocal = new osc.UDPPort({
 	localAddress: "localhost",
-	localPort: 5002,
+	localPort: process.env.LANPORT,
 	metadata: true,
 });
 udpPortLocal.open()
@@ -58,43 +55,41 @@ function sendOSCMessage(clientIP, ip, port, addressp, type, value) {
 	};
 	console.log("Sending message", msg.address, msg.args, "to", ip + ":" + port);
 
-	if(ip == "localhost"){
+	if (ip == "localhost") {
 		//ip = clientIP;
-		try{
+		try {
 			udpPortLocal.send(msg, ip, port);
 		} catch (err) {
 			console.log("chatching this error")
 			console.log("ERROR: ", err)
 		}
 	}
-	else{
-	try{
-		udpPortGlobal.send(msg, ip, port);
-	} catch (err) {
-		console.log("chatching this error")
-		console.log("ERROR: ", err)
+	else {
+		try {
+			udpPortGlobal.send(msg, ip, port);
+		} catch (err) {
+			console.log("chatching this error")
+			console.log("ERROR: ", err)
+		}
 	}
 }
-}
-
 io.sockets.on('connection', function (socket) {
 	console.log('Web sockect is connected between OSCAR and Local Server');
-	socket.on("config", function (obj) {
-		console.log("Receive Config: ", obj.client.host, obj.client.port)
-		var device = { ip: obj.client.host, port: obj.client.port }
-		oscConnections.push(device);
-		isConnected.push(true);
-	})
+	// socket.on("config", function (obj) {
+	// 	console.log("Receive Config: ", obj.client.host, obj.client.port)
+	// 	var device = { ip: obj.client.host, port: obj.client.port }
+	// 	oscConnections.push(device);
+	// 	isConnected.push(true);
+	// })
 	socket.on("message", function (clientIP, ip, port, addressp, type, value) {
-			try {
-				sendOSCMessage(clientIP, ip, port, addressp, type, value);
-			}
-			catch (err) {
-				console.log("ERROR: ", err)
-			}
+		try {
+			sendOSCMessage(clientIP, ip, port, addressp, type, value);
+		}
+		catch (err) {
+			console.log("ERROR: ", err)
+		}
 	});
 	socket.on("code", function (obj) {
-		//console.log(code);
 		code = obj;
 	});
 })
@@ -112,14 +107,13 @@ app.get('/preview', function (req, res) {
 app.get('/dom', function (req, res) {
 	console.log("Requesting DOM", code)
 	res.send(code)
-}) 
+})
 app.get('/ipserver', function (req, res) {
 	res.send(serverIP)
 })
 // app.listen(8080, config.ip, function () {	
 // })
-app.listen(8080, function () {
-	
+app.listen(process.env.HTTPPORT, function () {
 })
 
 
